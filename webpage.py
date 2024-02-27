746204466a06bdac35e01318b7676b0f1c617413
Fardeen2903 <64099161+Fardeen2903@users.noreply.github.com>
2024-02-27 11:35:08 -0500

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -166,5 +166,5 @@ def delete_person(person_id):
     return redirect(url_for('view_people'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
