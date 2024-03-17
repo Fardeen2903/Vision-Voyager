54c3e757ffd73f14eaa768f548ef9ebc199ed875
Fardeen2903 <64099161+Fardeen2903@users.noreply.github.com>
2024-03-17 19:14:51 -0400

    Update EncodeGenerator.py

--- Face-Rec/EncodeGenerator.py
+++ Face-Rec/EncodeGenerator.py
@@ -1,10 +1,9 @@
+import os
+import pickle
 import cv2
 import face_recognition
-import pickle
-import os
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import db
 from firebase_admin import storage
 
 cred = credentials.Certificate("VVDB_KEY.json")
@@ -13,25 +12,27 @@ firebase_admin.initialize_app(cred, {
     'storageBucket': "visionvoyagerdb.appspot.com"
 })
 
-# Importing student images
 folderPath = 'Images'
 pathList = os.listdir(folderPath)
 print(pathList)
+
 imgList = []
 studentIds = []
+
 for path in pathList:
-    imgList.append(cv2.imread(os.path.join(folderPath, path)))
-    studentIds.append(os.path.splitext(path)[0])
+    img = cv2.imread(os.path.join(folderPath, path))
+    if img is not None:
+        imgList.append(img)
+        studentIds.append(os.path.splitext(path)[0])
 
-    # Adding Da Images
-    fileName = f'{folderPath}/{path}'
-    bucket = storage.bucket()
-    blob = bucket.blob(fileName) # Send to DB...hopefully
-    blob.upload_from_filename(fileName)
+        # Adding Images
+        fileName = f'{folderPath}/{path}'
+        bucket = storage.bucket()
+        blob = bucket.blob(fileName)
+        blob.upload_from_filename(fileName)
+    else:
+        print(f"Failed to read image: {path}")
 
-
-    # print(path)
-    # print(os.path.splitext(path)[0])
 print(studentIds)
 
 
@@ -39,8 +40,12 @@ def findEncodings(imagesList):
     encodeList = []
     for img in imagesList:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
+        face_encodings = face_recognition.face_encodings(img)
+
+        if face_encodings:
+            encodeList.append(face_encodings[0])
+        else:
+            print(f"No face found in image: {img}")
 
     return encodeList
 
@@ -50,7 +55,7 @@ encodeListKnown = findEncodings(imgList)
 encodeListKnownWithIds = [encodeListKnown, studentIds]
 print("Encoding Complete")
 
-file = open("EncodeFile.p", 'wb')
+file = open("Face-Rec/EncodeFile.p", 'wb')
 pickle.dump(encodeListKnownWithIds, file)
 file.close()
 print("File Saved")
